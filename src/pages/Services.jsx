d01a8ca50422dc9db7e4bcffdaa5f548d9024e45
Mihai <cataraga.95@gmail.com>
2024-05-10 16:27:09 +0300

    new

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,14 +4,59 @@ import SecNavbar from '../components/SecNavbar'
 import card from '../data/servicesCard'
 
 export default function Services() {
+    // const [cardNum, setCardNum] = useState(0);
+    
+    // const dot = () => {
+    //     const elem = document.getElementById(`card1`)
+    //     // elem.style.animationDelay = '5s'
+    //     elem.style.animation = 'none'
+    //     elem.style.marginTop = `calc(-60vh * ${cardNum})`
+    // }
 
+    // useEffect(() => {
+    //     const testElement = document.getElementById('test');
+    //     testElement.style.transition = '1s'
+    //     let scrollPosition = 0;
+    //     const interval = setInterval(() => {
+    //         // Actualizăm poziția scroll-ului
+    //         scrollPosition += 400;
+
+    //         // Dacă am ajuns la sfârșitul conținutului, revenim la început
+    //         if (scrollPosition >= testElement.scrollHeight - testElement.clientHeight) {
+    //             scrollPosition = 0;
+    //         }
+
+    //         // Setăm noua poziție a scroll-ului
+    //         testElement.scrollTop = scrollPosition;
+    //     }, 2000)
+
+    //     return () => {
+    //         clearInterval(interval)
+    //     }
+    // }, [])
+
+    // useEffect(() => {
+    //     dot()
+    //     const time = cardNum === 7 ? 0 : 5000
+    //     const timeout = setTimeout(() => {
+
+    //         const elem = document.getElementById(`card1`)
+    //         elem.style.animation = '35s card linear infinite'
+    //         // elem.style.marginTop = `calc(-60vh * ${cardNum})`
+    //     }, time)
+        
+    //     console.log(cardNum)
+    //     return () => {
+    //         clearTimeout(timeout)
+    //     }
+    // }, [cardNum])
   return (
     <div className='services'>
         <Navbar />
         <main>
             <SecNavbar page='Services' />
             <section className='sec-1'>
-                <h1 id='h1'>What types of websites do we realize?</h1>
+                <h1>What types of websites do we realize?</h1>
                 <div className='servicesType'>
                     <div className='serviceCard'>
                         {card.map((i) => {
@@ -38,9 +83,23 @@ export default function Services() {
                     </div>
                 </div>
             </section>
-            <section className='sec-2'>
-                fdgsbdfgs
-            </section>
+            <div className='test' id='test'>
+                <div className='div1'>
+
+                </div>
+                <div className='div2'>
+
+                </div>
+                <div className='div3'>
+
+                </div>
+                <div className='div4'>
+
+                </div>
+                <div className='div5'>
+
+                </div>
+            </div>
         </main>
     </div>
   )
